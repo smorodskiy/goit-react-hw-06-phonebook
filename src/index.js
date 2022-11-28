832b50e46eba1656f2e381bf13171332d884000f
Igor Smorodsky <ingerard@gmail.com>
2022-11-28 20:00:15 +0200

    provider for app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
+
+// Theme
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'constants';
 
+// Redux provider
+import { Provider } from 'react-redux';
+
 // Normalize style
 import GlobalStyle from './globalStyle';
+
+// Redux store
+import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
