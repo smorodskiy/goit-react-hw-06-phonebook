b80de8f5f388792304a54fa8e2320b26c729814b
Igor Smorodsky <ingerard@gmail.com>
2022-12-17 11:21:07 -0800

    configured store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,35 +1,28 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
 
-const initialState = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
-  filter: '',
-};
+// const rootReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'contacts/loadFromStore':
+//       return { ...state, contacts: action.payload };
+//     case 'contacts/add':
+//       return { ...state, contacts: [...state.contacts, action.payload] };
+//     case 'contacts/delete':
+//       return {
+//         ...state,
+//         contacts: state.contacts.filter(({ id }) => id !== action.payload),
+//       };
+//     case 'contacts/filter':
+//       return { ...state, filter: action.payload };
+//     default:
+//       return state;
+//   }
+// };
 
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'contacts/loadFromStore':
-      return { ...state, contacts: action.payload };
-    case 'contacts/add':
-      return { ...state, contacts: [...state.contacts, action.payload] };
-    case 'contacts/delete':
-      return {
-        ...state,
-        contacts: state.contacts.filter(({ id }) => id !== action.payload),
-      };
-    case 'contacts/filter':
-      return { ...state, filter: action.payload };
-    default:
-      return state;
-  }
-};
-
-// Створюємо розширення стора, щоб додати інструменти розробника
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+});
