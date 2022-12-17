db3a9c05a1e9fc9d8b07e3f8d7358c4e2d7e80ee
Igor Smorodsky <ingerard@gmail.com>
2022-12-17 13:12:45 -0800

    persist reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,27 +2,38 @@ import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
-// const rootReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case 'contacts/loadFromStore':
-//       return { ...state, contacts: action.payload };
-//     case 'contacts/add':
-//       return { ...state, contacts: [...state.contacts, action.payload] };
-//     case 'contacts/delete':
-//       return {
-//         ...state,
-//         contacts: state.contacts.filter(({ id }) => id !== action.payload),
-//       };
-//     case 'contacts/filter':
-//       return { ...state, filter: action.payload };
-//     default:
-//       return state;
-//   }
-// };
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
+    persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
