c78d6c37a9ff7f13f6cc1261f30066e5b9a4942a
Igor Smorodsky <ingerard@gmail.com>
2022-12-17 13:08:23 -0800

    del extra logic

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 // Components
 import { Section, Phonebook, Contacts, Filter } from 'components';
 
@@ -8,22 +7,14 @@ import { Section, Phonebook, Contacts, Filter } from 'components';
 import { Container } from './App.styled';
 
 export const App = () => {
- 
-
-  // On input filter
-  const handleInputFilter = e => {
-    const inputValue = e.target.value;
-    // dispatch(filtersContact(inputValue));
-  };
-
   return (
     <Container>
       <Section title="Phonebook">
-        <Phonebook/>
+        <Phonebook />
       </Section>
 
       <Section title="Contacts">
-        <Filter onInputFilter={handleInputFilter} />
+        <Filter />
       </Section>
 
       <Contacts />
